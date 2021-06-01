d40a0830d2a0666562358df70c5a4b280bd7f619
ar-mine <934218777@qq.com>
2021-06-01 21:06:58 +0800

    object_detection v1.1

--- vision/object_detection/README.md
+++ vision/object_detection/README.md
@@ -1,6 +1,28 @@
 # Version 1.0
 1. Finish basic object detection function with yolo_v5.
-2. Subscriber: /camera/color/image_raw
-   Publisher : /object_detection/image_anchor
-3. Reference: yolo_v5: https://github.com/ultralytics/yolov5 
-4. TODO: Solve the real-time problem when the task is heavy(print the stamp and get_time() to see)
+2. - Subscriber: ```/camera/color/image_raw```
+   - Publisher : ```/object_detection/image_anchor```
+3. Reference: [yolo_v5 github](https://github.com/ultralytics/yolov5),
+              [coco-2017 class description](https://tech.amikelive.com/node-718/what-object-categories-labels-are-in-coco-dataset/)
+### TODO
+Solve the real-time problem when the task is heavy(print the stamp and get_time() to see)
+***
+# Version 1.1
+### Change
+1. Update ```opencv_vis``` flag for open and close the function of window visualization
+2. Add word labels on the detection results
+3. It can give the center of detected object as ```Marker``` now
+4. Update README.md format   
+5. Get intrinsic matrix and distortion parameters automatically
+#### Subscriber
+* /camera/color/image_raw
+* /camera/aligned_depth_to_color/image_raw
+* /camera/aligned_depth_to_color/camera_info 
+#### Publisher
+* /object_detection/image_anchor
+* /object_detection/markers
+### Tips
+1. The topic ```/camera/aligned_depth_to_color/image_raw``` is published on the ```camera_color_optical_frame``` rather than the ```camera_aligned_depth_to_color_frame```
+### TODO
+Beautify the window visualization when ```opencv_vis==True```
+***
--- vision/object_detection/script/object_detection_node.py
+++ vision/object_detection/script/object_detection_node.py
@@ -1,17 +1,61 @@
+# System Lib
+import threading
 # 3rd-party lib
 import numpy as np
 import cv2
 # pytorch
 import torch
-import torchvision.transforms as transforms
 # ros
 import rospy
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
+from visualization_msgs.msg import Marker, MarkerArray
+from sensor_msgs.msg import Image, CameraInfo
+from cv_bridge import CvBridge, CvBridgeError
+
+
+def loop():
+    while not rospy.is_shutdown():
+        rospy.spin()
+
+
+def marker_gen(frame_id, class_name, idx, score, pos):
+    marker = Marker()
+
+    marker.header.stamp = rospy.Time.now()
+    marker.header.frame_id = frame_id
+    marker.ns = class_name
+    marker.id = idx
+    marker.type = marker.SPHERE
+    marker.action = marker.ADD
+
+    marker.pose.position.x = pos[0]
+    marker.pose.position.y = pos[1]
+    marker.pose.position.z = pos[2]
+
+    marker.pose.orientation.w = 1.0
+
+    marker.scale.x = 0.1
+    marker.scale.y = 0.1
+    marker.scale.z = 0.1
+
+    hsv = np.uint8([[[score * 180, 255, 255]]])
+    color = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)[0][0]
+
+    marker.color.a = 0.5
+    marker.color.r = color[0] / 255.0
+    marker.color.g = color[1] / 255.0
+    marker.color.b = color[2] / 255.0
+
+    return marker
 
 
 class ObjectDetection:
-    def __init__(self):
+    def __init__(self, opencv_vis=False):
+        # === Class parameters ===
+        self.opencv_vis = opencv_vis
+        subs_img_topic = "/camera/color/image_raw"
+        subs_img_d_topic = "/camera/aligned_depth_to_color/image_raw"
+        subs_cam_info_topic = "/camera/aligned_depth_to_color/camera_info"
+
         # === Vision model init ===
         self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
         # load a model pre-trained pre-trained on COCO
@@ -19,45 +63,102 @@ class ObjectDetection:
         self.model.to(self.device)
         # Only for evaluation
         self.model.eval()
-        self.transf = transforms.ToTensor()
 
-        # Publisher and Subscriber
+        # === ROS init ===
+        self.lock = threading.RLock()
+        with self.lock:
+            self.depth_img = Image()
+            self.camera_info = CameraInfo()
         self.bridge = CvBridge()
-        self.img_sub = rospy.Subscriber("/camera/color/image_raw", Image, self.callback, queue_size=1, buff_size=2**22)
+        self.img_sub = rospy.Subscriber(subs_img_topic, Image, self.callback, queue_size=1, buff_size=2**22)
+        self.img_d_sub = rospy.Subscriber(subs_img_d_topic, Image, self.depth_trans, queue_size=1, buff_size=2**21)
+        self.cam_info_sub = rospy.Subscriber(subs_cam_info_topic, CameraInfo, self.intrinsic_trans, queue_size=1)
         self.img_pub = rospy.Publisher("/object_detection/image_anchor", Image, queue_size=1)
+        self.markers_pub = rospy.Publisher("/object_detection/markers", MarkerArray, queue_size=1)
 
         # === Main Loop ===
-        self.loop()
-
-    def loop(self):
-        while not rospy.is_shutdown():
-            rospy.spin()
+        loop()
 
     def callback(self, img_msg):
-        cv_image = self.bridge.imgmsg_to_cv2(img_msg, "bgr8")
-        cv_image = cv_image[342:, 374:, :]
-        image_rgb = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+        with self.lock:
+            img_d_msg = self.depth_img
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(img_msg, "bgr8")
+            cv_image_d = self.bridge.imgmsg_to_cv2(img_d_msg)
+        except CvBridgeError as e:
+            print(e)
+            return
+
+        # Clip the image to improve the efficiency for ROI
+        bias_y = 342
+        bias_x = 374
+        # Note the x and y are inverse
+        image_rgb = cv2.cvtColor(cv_image[bias_y:, bias_x:, :], cv2.COLOR_BGR2RGB)
 
         predictions = self.model([image_rgb], size=640)
 
         # Parameters
         pred = predictions.xyxy[0].cpu()
         boxes = pred[:, 0:4].detach().numpy().astype(np.int64)
+        boxes[:, 0] += bias_x
+        boxes[:, 1] += bias_y
+        boxes[:, 2] += bias_x
+        boxes[:, 3] += bias_y
         scores = pred[:, 4].detach().numpy()
         labels = pred[:, 5].detach().numpy().astype(np.int64)
 
+        markers = MarkerArray()
         # Visualization
-        labels_name = []
         for idx, box in enumerate(boxes):
-            if scores[idx] >= 0.0:
-                cv2.rectangle(cv_image, box[0:2], box[2:], (255, 0, 0))
-                labels_name.append(predictions.names[labels[idx]])
+            class_name = predictions.names[labels[idx]]
+            cv2.rectangle(cv_image, box[0:2], box[2:], (255, 0, 0), 1)
+            cv2.rectangle(cv_image, box[0:2], (box[0] + 80, box[1] + 20), (255, 0, 0), -1)
+            cv2.putText(cv_image, class_name, (box[0] + 6, box[1] + 16), cv2.FONT_HERSHEY_PLAIN, 1,
+                        (255, 255, 255), 1, cv2.LINE_AA)
 
-        cv2.imshow("object_detection", cv_image)
-        cv2.waitKey(1)
+            frame_id = img_msg.header.frame_id
+            pos = [(box[0]+box[2])//2, (box[1]+box[3])//2]
+            depth = cv_image_d[pos[1], pos[0]]*1e-3
+            # print(class_name, self.de_projection(pos, depth))
+            markers.markers.append(marker_gen(frame_id, class_name, idx, scores[idx], self.de_projection(pos, depth)))
+
+        if self.opencv_vis:
+            cv2.imshow("object_detection", cv_image)
+            cv2.waitKey(1)
 
         img_msg_pub = self.bridge.cv2_to_imgmsg(cv_image, "bgr8")
+        img_msg_pub.header.frame_id = img_msg.header.frame_id
         self.img_pub.publish(img_msg_pub)
+        self.markers_pub.publish(markers)
+
+    def depth_trans(self, msg):
+        with self.lock:
+            self.depth_img = msg
+
+    def intrinsic_trans(self, msg):
+        with self.lock:
+            self.camera_info = msg
+
+    def de_projection(self, pixel, depth):
+        with self.lock:
+            cam_info = self.camera_info
+        coeffs = cam_info.D
+        ppx = cam_info.K[2]
+        ppy = cam_info.K[5]
+        fx = cam_info.K[0]
+        fy = cam_info.K[4]
+
+        x = (pixel[0] - ppx) / fx
+        y = (pixel[1] - ppy) / fy
+
+        r2 = x * x + y * y
+        f = 1 + coeffs[0] * r2 + coeffs[1] * r2 * r2 + coeffs[4] * r2 * r2 * r2
+        ux = x * f + 2 * coeffs[2] * x * y + coeffs[3] * (r2 + 2 * x * x)
+        uy = y * f + 2 * coeffs[3] * x * y + coeffs[2] * (r2 + 2 * y * y)
+        x = ux
+        y = uy
+        point = [depth * x, depth * y, depth]
+        return point
 
 
 if __name__ == "__main__":
